f23dce264d6d967740468929dd917805186648ca
Angel-Avila589 <avila.angel217@gmail.com>
2021-11-12 10:17:34 -0800

    incomplete test.cpp

--- test.cpp
+++ test.cpp
@@ -1,6 +1,37 @@
 #include "spreadsheet.hpp"
 
 #include "gtest/gtest.h"
+#include "select.hpp"
+#include "select_and.hpp"
+#include "select_or.hpp"
+#include "select_contains.hpp"
+#include "select_not.hpp"
+#include <iostream>
+
+TEST(SeletctNotTest, TestingSelectNot)
+{
+	Spreadsheet sheet;
+	sheet.set_column_names({"First","Last","Age", "Major"});
+	sheet.add_row({"Amanda","Andrews","22","business" });
+	sheet.add_row({ "Brian","Becker","21","computer science" });
+	sheet.add_row({ "Carol","Conners","21","computer science" });
+	Select* tst = new Select_Not(new Select_Contains(&sheet, "Last", "m"));
+	bool retVal = tst->select(&sheet, 0);
+	EXPECT_EQ(retVal, true);
+
+	delete tst;
+
+	tst = new Select_Not(new Select_Contains(&sheet, "First", "Brian"));
+	retVal = tst->select(&sheet, 1);
+	EXPECT_EQ(retVal, false);
+
+	delete tst;
+}
+
+TEST(SelectAndTest, TestingSelectAnd)
+{
+	
+}
 
 
 int main(int argc, char **argv) {
